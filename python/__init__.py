2e0d74ac91976ce0bfdc9ffff2d84146f333ede9
artemies <artemies@users.noreply.github.com>
2021-03-18 14:39:43 +0800

    Update __init__.py

--- python/__init__.py
+++ python/__init__.py
@@ -36,6 +36,8 @@ from pdu_to_kiss import pdu_to_kiss
 from hdlc_framer import hdlc_framer
 from nrzi_encode import nrzi_encode
 from nrzi_decode import nrzi_decode
+from nrzs_encode import nrzs_encode
+from nrzs_decode import nrzs_decode
 from hdlc_deframer import hdlc_deframer
 from check_address import check_address
 #
